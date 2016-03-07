9f7350371c585e80483e0d6112658961d972a2b4
wushuiyong <wu.shuiyong@qq.com>
2016-03-07 10:35:00 +0800

    url

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -100,9 +100,9 @@ hexo.extend.helper.register('url_for_lang', function(path){
 hexo.extend.helper.register('raw_link', function(path){
   console.log('raw'+path);
   //path = this.page.lang + '/' + path.replace(/master\/walle-web.io\/docs\/((zh\-cn|en)\/)?/, '');
-  path = path.replace(/master\/walle-web.io\/docs\/((zh\-cn|en)\/)?/, '');
+  path = path.replace(/docs\/((zh\-cn|en)\/)?/, '');
 
-  return 'https://github.com/meolu/docs/edit/' + path.replace('//', '/');
+  return 'https://github.com/meolu/docs/edit/master/walle-web.io/' + path.replace('//', '/');
 });
 
 hexo.extend.helper.register('page_anchor', function(str){
